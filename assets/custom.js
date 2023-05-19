5b0e7b66063769619e42892d87645c3e6dbc4e40
shen0402 <shenzaijun0402@gmail.com>
2023-05-19 14:56:07 -0700

    update job-fairs-and-events page

--- assets/custom.js
+++ assets/custom.js
@@ -4,11 +4,40 @@ $(document).on('click', '.job-filter-link', function(e){
     const q = $(this).data('query').trim();
     $('.dropdown-filter-list').removeClass('w--open');
     $('.job-filter-toggle__text').text(label);
+    $('.job-individ-wrapper').hide();
     if (q) {
-        $('.job-individ-wrapper').hide();
         $(`[data-job-type="${q}"]`).fadeIn();
     } else {
-        $('.job-individ-wrapper').hide();
         $('.job-individ-wrapper').fadeIn();
+    }
+});
+
+$(document).on('click', '.job-filter-link2', function(e){
+    e.preventDefault();
+    $(this).closest('.dropdown-filter-list').find('.job-filter-link2').removeClass('active');
+    $(this).addClass('active');
+    $(this).closest('.dropdown-filter-list').removeClass('w--open');
+    const label = $(this).text().trim();
+    $(this).closest('.filter-dropdown').find('.job-filter-toggle__text').text(label);
+    const organization = $('.dropdown-filter-list__organization .active').data('filter-organization');
+    const location = $('.dropdown-filter-list__location .active').data('filter-location');
+    const month = $('.dropdown-filter-list__month .active').data('filter-month');
+    $('.job-fair-individ-wrapper').hide();
+    if (organization && location && month) {
+        $(`[data-query-organization="${organization}"][data-query-location="${location}"][data-query-month="${month}"]`).fadeIn();
+    } else if (organization && location) {
+        $(`[data-query-organization="${organization}"][data-query-location="${location}"]`).fadeIn();
+    } else if (location && month) {
+        $(`[data-query-location="${location}"][data-query-month="${month}"]`).fadeIn();
+    } else if (organization && month) {
+        $(`[data-query-organization="${organization}"][data-query-month="${month}"]`).fadeIn();
+    } else if (organization) {
+        $(`[data-query-organization="${organization}"]`).fadeIn();
+    } else if (location) {
+        $(`[data-query-location="${location}"]`).fadeIn();
+    } else if (month) {
+        $(`[data-query-month="${month}"]`).fadeIn();
+    } else {
+        $('.job-fair-individ-wrapper').fadeIn();
     }
 });
